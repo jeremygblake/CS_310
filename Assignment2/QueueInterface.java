12787589afd83e1f5d3d0cd5531db7c384b99267
Jeremy <jeremygblake@users.noreply.github.com>
2019-09-30 15:20:24 -0700

    +

--- Assignment2/QueueInterface.java
+++ Assignment2/QueueInterface.java
@@ -2,4 +2,6 @@ public interface QueueInterface{
   public void enqueue(Object o);
   public Object dequeue();
   public Object peek();
+  public boolean isFull();
+  public boolean isEmpty();
 }
--- Assignment2/StudentRegistration.java
+++ Assignment2/StudentRegistration.java
@@ -10,22 +10,18 @@ public class StudentRegistration
 {
   public static void main(String[] args) throws FileNotFoundException, IOException
   {
-
-
-    FileReader file = new FileReader("Records.txt");
-    BufferedReader br = new BufferedReader(file);
-
-    int numberOfLines = 0;
+    FileReader      file = new FileReader("Records.txt");
+    BufferedReader  br = new BufferedReader(file);
+    StudentInfo[]   si_arr = new StudentInfo[numberOfLines];
+    Scanner         sysIn = new Scanner(System.in);
+    StudentInfo[]   enrolledStudents = null;
+    int             numberOfLines = 0;
+    String          record = "";
 
     while(br.readLine() != null)
     {
       numberOfLines++;
     }
-
-    StudentInfo[] si_arr = new StudentInfo[numberOfLines];
-
-    String record = "";
-    Scanner sysIn = new Scanner(System.in);
 
     int i = 0;
     while((record = br.readLine()) != null)
@@ -34,12 +30,26 @@ public class StudentRegistration
       si_arr[i] = new StudentInfo(nameThenId[0], Integer.parseInt(nameThenId[1]));
     }
 
-
     Scanner in = new Scanner(System.in);
     System.out.println("Enter the number of seats in the class: ");
     int seats = in.nextInt();
+    if(seats < numberOfLines)
+    {
+      System.out.println("Not enough seats to fill from file");
+      return;
+    }
 
-    
+    enrolledStudents = new StudentInfo[seats];
+
+    for(int i = 0; i < si_arr.length; i++)
+    {
+      enrolledStudents[i] = si_arr[i];
+    }
+
+    Queue
+
+
+
 
 
 
