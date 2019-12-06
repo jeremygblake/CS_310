f9e77e0dea201a8d40323c843d2da985cce951c9
Jeremy <jeremygblake@users.noreply.github.com>
2019-12-05 23:01:19 -0800

    +

--- Hw3/src/com/company/ChainingHashTable.java
+++ Hw3/src/com/company/ChainingHashTable.java
@@ -1,4 +1,32 @@
 package com.company;
 
-public class ChainingHashTable {
+public class ChainingHashTable implements MapInterface < K, V > {
+
+    public V get(K k) {
+        return null;
+    }
+
+    public V put(K k, V v) {
+        return null;
+    }
+
+    public V remove(K k) {
+        return null;
+    }
+
+    public int sie() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public K[] keys() {
+        return new K[0];
+    }
+
+    public V[] values() {
+        return new V[0];
+    }
 }
